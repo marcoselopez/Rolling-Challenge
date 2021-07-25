d41dfac319fecc1cd383324732856869ba012b8b
unknown <emmanuel.m.lopez@gmail.com>
2021-07-24 23:12:20 -0300

    starting work on the portfolio section

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 const toggle = document.querySelector('#navProfileMenu')
 const items = document.querySelectorAll('#navItem')
+const projectTopItem = document.styleSheets[3].cssRules[45].style
+const projectBottomItem = document.styleSheets[3].cssRules[46].style
+const projectbox1 = document.querySelector('#projectBox1')
+console.log(projectTopItem)
 
-// NOTE TOGGLE MENU WHEN CLICK ON MENU
+//NOTE TOGGLE MENU WHEN CLICK ON MENU
 function toggleMenu(){
   let checkItem;
   items.forEach(item => {
@@ -23,11 +27,25 @@ function toggleMenu(){
 
 //NOTE CLOSE MENU WHEN CLICK ON LINK
 function toggleMenuDelayed(){
-  var timer = setTimeout(function(){
+  let timer = setTimeout(function(){
     items.forEach(item => item.classList.remove("active"))
     toggle.innerHTML = "menu"
     return timer;
   }, 400)
+}
+
+//NOTE CONTENT APPEARS AND DISSAPPEARS WHEN HOVER
+function hoverToggleOn(){
+  projectTopItem.removeProperty('display')
+  projectTopItem.setProperty('display', 'flex')
+  projectBottomItem.removeProperty('display')
+  projectBottomItem.setProperty('display', 'flex')  
+}
+function hoverToggleOff(){
+  projectTopItem.removeProperty('display')
+  projectTopItem.setProperty('display', 'none')
+  projectBottomItem.removeProperty('display')
+  projectBottomItem.setProperty('display', 'none')  
 }
 
 //NOTE EVENT LISTENERS
@@ -35,4 +53,5 @@ toggle.addEventListener('click', toggleMenu, false);
 items.forEach(item => {
   item.addEventListener('click', toggleMenuDelayed, false);
 })
-
+projectBox1.addEventListener('mouseenter', hoverToggleOn, false)
+projectbox1.addEventListener('mouseleave', hoverToggleOff, false)
