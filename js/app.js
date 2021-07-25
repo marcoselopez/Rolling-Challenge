b5672bc9cd2bd2877c220886876111afa757b963
unknown <emmanuel.m.lopez@gmail.com>
2021-07-25 15:38:50 -0300

    Finishing on projects section and starting on footer

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 const toggle = document.querySelector('#navProfileMenu')
 const items = document.querySelectorAll('#navItem')
-const projectTopItem = document.styleSheets[3].cssRules[45].style
-const projectBottomItem = document.styleSheets[3].cssRules[46].style
+//NOTE PROJECT ITEMS 1
+const projectTopItem1 = document.styleSheets[3].cssRules[47].style
+const projectBottomItem1 = document.styleSheets[3].cssRules[48].style
 const projectbox1 = document.querySelector('#projectBox1')
-console.log(projectTopItem)
+//NOTE PROJECT ITEMS 2
+const projectTopItem2 = document.styleSheets[3].cssRules[49].style
+const projectBottomItem2 = document.styleSheets[3].cssRules[50].style
+const projectbox2 = document.querySelector('#projectBox2')
+//NOTE PROJECT ITEMS 3
+const projectTopItem3 = document.styleSheets[3].cssRules[51].style
+const projectBottomItem3 = document.styleSheets[3].cssRules[52].style
+const projectbox3 = document.querySelector('#projectBox3')
+
+console.log(document.styleSheets[3].cssRules)
 
 //NOTE TOGGLE MENU WHEN CLICK ON MENU
 function toggleMenu(){
@@ -34,18 +44,46 @@ function toggleMenuDelayed(){
   }, 400)
 }
 
-//NOTE CONTENT APPEARS AND DISSAPPEARS WHEN HOVER
-function hoverToggleOn(){
-  projectTopItem.removeProperty('display')
-  projectTopItem.setProperty('display', 'flex')
-  projectBottomItem.removeProperty('display')
-  projectBottomItem.setProperty('display', 'flex')  
+//NOTE CONTENT APPEARS AND DISSAPPEARS WHEN HOVER - PROJECT BOX 1
+function hoverToggleOn1(){
+  projectTopItem1.removeProperty('display')
+  projectTopItem1.setProperty('display', 'flex')
+  projectBottomItem1.removeProperty('display')
+  projectBottomItem1.setProperty('display', 'flex')  
 }
-function hoverToggleOff(){
-  projectTopItem.removeProperty('display')
-  projectTopItem.setProperty('display', 'none')
-  projectBottomItem.removeProperty('display')
-  projectBottomItem.setProperty('display', 'none')  
+function hoverToggleOff1(){
+  projectTopItem1.removeProperty('display')
+  projectTopItem1.setProperty('display', 'none')
+  projectBottomItem1.removeProperty('display')
+  projectBottomItem1.setProperty('display', 'none')  
+}
+
+//NOTE CONTENT APPEARS AND DISSAPPEARS WHEN HOVER - PROJECT BOX 2
+function hoverToggleOn2(){
+  projectTopItem2.removeProperty('display')
+  projectTopItem2.setProperty('display', 'flex')
+  projectBottomItem2.removeProperty('display')
+  projectBottomItem2.setProperty('display', 'flex')  
+}
+function hoverToggleOff2(){
+  projectTopItem2.removeProperty('display')
+  projectTopItem2.setProperty('display', 'none')
+  projectBottomItem2.removeProperty('display')
+  projectBottomItem2.setProperty('display', 'none')  
+}
+
+//NOTE CONTENT APPEARS AND DISSAPPEARS WHEN HOVER - PROJECT BOX 3
+function hoverToggleOn3(){
+  projectTopItem3.removeProperty('display')
+  projectTopItem3.setProperty('display', 'flex')
+  projectBottomItem3.removeProperty('display')
+  projectBottomItem3.setProperty('display', 'flex')  
+}
+function hoverToggleOff3(){
+  projectTopItem3.removeProperty('display')
+  projectTopItem3.setProperty('display', 'none')
+  projectBottomItem3.removeProperty('display')
+  projectBottomItem3.setProperty('display', 'none')  
 }
 
 //NOTE EVENT LISTENERS
@@ -53,5 +91,9 @@ toggle.addEventListener('click', toggleMenu, false);
 items.forEach(item => {
   item.addEventListener('click', toggleMenuDelayed, false);
 })
-projectBox1.addEventListener('mouseenter', hoverToggleOn, false)
-projectbox1.addEventListener('mouseleave', hoverToggleOff, false)
+projectBox1.addEventListener('mouseenter', hoverToggleOn1, false)
+projectbox1.addEventListener('mouseleave', hoverToggleOff1, false)
+projectBox2.addEventListener('mouseenter', hoverToggleOn2, false)
+projectbox2.addEventListener('mouseleave', hoverToggleOff2, false)
+projectBox3.addEventListener('mouseenter', hoverToggleOn3, false)
+projectbox3.addEventListener('mouseleave', hoverToggleOff3, false)
